b874a44043e6e776c4f44393529fdd45b853b1dd
zack <zcdorman@gmail.com>
2024-04-09 17:01:23 +0900

    connect pull request and asana in index

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const pull = require("./pull");
+const { createTask } = require('./asana');
 
-try {
-    pull.getPullRequestInfo();
-} catch (error) {
-    core.setFailed(error.message);
+
+async function run() {
+    try {
+        let pullRequestInfoArray = await pull.getFilesWithTodoComments();
+
+        pullRequestInfoArray.forEach(pullRequestInfo => {
+            let fileName = pullRequestInfo.fileName;
+            pullRequestInfo.codeBlocks.forEach((codeBlock, index) => {
+                createTask(
+                    `${fileName}: ${index} (${new Date().toLocaleDateString()})`,
+                    codeBlock,
+                )
+            });
+        });
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
+
+run();
