d510748aea72a3492b56e96185629d873307d292
zack <zcdorman@gmail.com>
2024-04-09 17:00:37 +0900

    asana: get values directly from github

--- asana.js
+++ asana.js
@@ -1,30 +1,40 @@
 
 const Asana = require('asana');
+const core = require('@actions/core');
 
 /**
  * 
- * @param {String} accessToken Asana PAT
  * @param {String} name Task name
  * @param {String} notes Notes to add to the ticket
- * @param {String array} projects Projects to add the task to
  * @returns String, url of created task
  */
-async function createTask(accessToken, name, notes, projects) {
+async function createTask(name, notes) {
     let client = Asana.ApiClient.instance;
     let token = client.authentications['token'];
+    let accessToken = core.getInput('asana-pat');
+    if (accessToken === null) {
+        throw new Error('Asana PAT has not been set! Please set the PAT in your YAML file.');
+    }
     token.accessToken = accessToken;
     
+    let asanaProjects = core.getInput('asana-projects');
+    if (asanaProjects === null) {
+        throw new Error('Asana project ids have not been set! Please set the project ids in your YAML file.');
+    }
+    console.log(`asanaProjects: ${asanaProjects}`)
+
     let tasksApiInstance = new Asana.TasksApi();
     let body = {
         "data": {
             "name": name,
             "notes": notes,
-            "projects": projects, // needs to be string array
+            "projects": asanaProjects, // needs to be string array
             "completed": false,
         },
     };
     let opts = {};
-    
+
+
     let resultUrl = '';
     // POST - Create a task
     await tasksApiInstance.createTask(body, opts).then((result) => {
