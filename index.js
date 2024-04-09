c73cc5fb09f1d79d04d56871d29d266a279eabc4
zack <zcdorman@gmail.com>
2024-04-09 20:09:29 +0900

    await fun

--- index.js
+++ index.js
@@ -8,23 +8,29 @@ async function run() {
     try {
         let pullRequestInfoArray = await pull.getFilesWithTodoComments();
 
-        let newTaskUrls = [];
-        pullRequestInfoArray.forEach(pullRequestInfo => {
-            let fileName = pullRequestInfo.fileName;
-            let pullRequestUrl = pullRequestInfo.pullRequestUrl;
-            pullRequestInfo.codeBlocks.forEach((codeBlock, index) => {
-                let taskName = `${fileName}: ${index +1} (${new Date().toLocaleDateString()})`;
-                let taskNote = `${pullRequestUrl}\n\n\`\`\`\n${codeBlock}\n\`\`\``;
-                let taskUrl = createTask(taskName, taskNote)
-                newTaskUrls.push(taskUrl);
-            });
-        });
+        let newTaskUrls = await makeAsanaTasks(pullRequestInfoArray);
+
         if(newTaskUrls.length > 0) {
             pull.postComment(`New Tasks:\n${newTaskUrls.join('\n')}`);
         }
     } catch (error) {
         core.setFailed(error.message);
     }
+}
+
+async function makeAsanaTasks(pullRequestInfoArray) {
+    let newTaskUrls = [];
+    await pullRequestInfoArray.forEach(pullRequestInfo => {
+        let fileName = pullRequestInfo.fileName;
+        let pullRequestUrl = pullRequestInfo.pullRequestUrl;
+        pullRequestInfo.codeBlocks.forEach((codeBlock, index) => {
+            let taskName = `${fileName}: ${index +1} (${new Date().toLocaleDateString()})`;
+            let taskNote = `${pullRequestUrl}\n\n\`\`\`\n${codeBlock}\n\`\`\``;
+            let taskUrl = createTask(taskName, taskNote)
+            newTaskUrls.push(taskUrl);
+        });
+    });
+    return newTaskUrls;
 }
 
 run();
